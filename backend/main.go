31190440817c2d24842c05e84219a52d61f9d2f2
Mikael Hachaturian <mikael.hach@gmail.com>
2022-12-07 15:15:11 +0200

    added user auth using jwt

---
+++ backend/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"backend/controllers"
+	"backend/middlewares"
+	"backend/models"
+
+	gin "github.com/gin-gonic/gin"
+)
+
+func init() {
+
+	models.ConnectDataBase()
+}
+
+func main() {
+
+	router := gin.Default()
+
+	// Health Check
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Ok"})
+	})
+
+	api := router.Group("/api")
+	{
+		api.POST("/user/register", controllers.Register)
+		api.POST("/user/login", controllers.Login)
+	}
+
+	authApi := router.Group("/api/auth")
+	{
+		authApi.Use(middlewares.JwtAuthMiddleware())
+		// protected.GET("/user", controllers.CurrentUser)
+		authApi.GET("/is-authenticated", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "yes"})
+		})
+	}
+
+	router.Run(":8081") // listen and serve on 0.0.0.0:8081
+}
