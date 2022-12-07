adb59464ea64af9b4668709b42b8978037563f0f
Mikael Hachaturian <mikael.hach@gmail.com>
2022-12-07 15:56:50 +0200

    Initial User Auth implmentation (#1)
    * added user auth using jwt
    
    * added Dockerfile and docker-compose
    
    * added Dockerfile and docker-compose
    
    * updates to README
    
    * remove env var to docker-compose

---
+++ backend/middlewares/middlewares.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"backend/utils/token"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := token.TokenValid(c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
